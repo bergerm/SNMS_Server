c65b3a39362110ad99bc89655c8bde9a285389ea
Manuel <manueldavidb@mail.afeka.ac.il>
2015-04-01 00:35:06 +0300

    A few Factories added

---
+++ SNMS_Server/Factories/PluginBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SNMS_Server.Plugins;
+
+namespace SNMS_Server.Factories
+{
+    class PluginBuilder
+    {
+        public static Plugin Build(string sFilePath, ref string sErrorString)
+        {
+            PluginParser parser = new PluginParser();
+
+            return parser.ParsePlugin(sFilePath, ref sErrorString);
+        }
+    }
+}
---
+++ SNMS_Server/Factories/PluginFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using SNMS_Server.Plugins;
+
+namespace SNMS_Server.Factories
+{
+    class PluginFactory
+    {
+        static List<Plugin> Build(string sPluginFilesFolderPath)
+        {
+            // TODO: Should download all plugins from DB here
+
+            string[] filePaths = Directory.GetFiles(sPluginFilesFolderPath);
+
+            string sErrorString = "";
+
+            List<Plugin> pluginList = new List<Plugin>();
+
+            foreach (string filePath in filePaths)
+            {
+                Plugin plugin = PluginBuilder.Build(filePath, ref sErrorString);
+
+                if (plugin != null)
+                {
+                    pluginList.Add(plugin);
+                }
+            }
+
+            return pluginList;
+        }
+    }
+}
